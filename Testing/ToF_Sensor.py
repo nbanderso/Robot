ec77a7414b96df2de065bdbfaeab43361cd8cd21
nbanderso <52547554+nbanderso@users.noreply.github.com>
2021-03-21 20:00:26 -0700

    Update ToF_Sensor.py

--- Testing/ToF_Sensor.py
+++ Testing/ToF_Sensor.py
@@ -5,7 +5,7 @@ import time
 #Rvr
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(_file_),'../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../../')))
 
 import RPi.GPIO as GPIO
 import asyncio
@@ -15,55 +15,56 @@ from sphero_sdk import SerialAsyncDal
 
 loop = asyncio.get_event_loop()
 rvr = SpheroRvrAsync(
-	dal=SerialAsyncDal(
-		loop
-	)
+    dal=SerialAsyncDal(
+        loop
+    )
 )
 
 #Testing Sparkfun ToF Sensor
 print("Testing qwiic sensor")
 ToF = qwiic.QwiicVL53L1X()
 if (ToF.SensorInit() == None):
-	print("Finally! not stuck!")
+    print("Finally! not stuck!")
 '''
 while True:
-	try:
-		ToF.StartRanging()
-		time.sleep(.05)
-		distance = ToF.GetDistance()
-		time.sleep(.05)
-		ToF.StopRanging()
+    try:
+        ToF.StartRanging()
+        time.sleep(.05)
+        distance = ToF.GetDistance()
+        time.sleep(.05)
+        ToF.StopRanging()
 
-		distanceInches = distance / 25.4
-		distanceFeet = distanceInches / 12.0
+        distanceInches = distance / 25.4
+        distanceFeet = distanceInches / 12.0
 
-	except Exception as e:
-		print(e)
+    except Exception as e:
+        print(e)
 '''
 
 ToF.StartRanging()
-## distance = ToF.GetDistance()
-## distanceInches = distance / 25.4
+# distance = ToF.GetDistance()
+# distanceInches = distance / 25.4
 
 async def main():
-	await rvr.wake()
-	await rvr.reset_yaw()
-	while True:
-		distance = ToF.GetDistance()
-		distanceInches = distance / 25.4
-		dist_f = int(distanceInches)
-		await asyncio(.05)
-		print('Distance is {0} inches'.format(dist_f))
-		if dist_f < 12:
-			while dist_f < 12:
-				await run_raw_motors(2,60,1,60)
-				await asyncio.sleep(.05)
-				print ('Turning Right')
-			await rvr.reset_yaw()
-		elif dist_f >= 12:
-			await run_raw_motors(1,60,1,60)
-			await asyncio.sleep(.05)
-			print('Going Straight')
+    await rvr.wake()
+    await rvr.reset_yaw()
+    while True:
+            distance = ToF.GetDistance()
+            distanceInches = distance / 25.4
+            dist_f = int(distanceInches)
+            print('Distance is {0} inches'.format(dist_f))
+            if dist_f < 12:
+                await rvr.raw_motors(2,150,1,150)
+                dist_f = int(distanceInches)
+                await asyncio.sleep(.5)
+                print ('Turning Right')
+                await rvr.reset_yaw()
+            elif dist_f >= 12:
+                await rvr.raw_motors(1,120,1,120)
+                dist_f = int(distanceInches)
+                await asyncio.sleep(.05)
+                await rvr.reset_yaw()
+
 
 try:
     loop.run_until_complete(
@@ -73,3 +74,4 @@ try:
     )
 except KeyboardInterrupt:
     print("Program ended by KeyboardInterrupt")
+
