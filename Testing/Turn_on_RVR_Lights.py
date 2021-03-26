5b545f98d81037941fd8f4fae7602e39d1c6ec4a
nbanderso <52547554+nbanderso@users.noreply.github.com>
2021-03-26 13:00:53 -0700

    Create Turn_on_RVR_Lights.py
    Turns on all Sphero RVR lights

---
+++ Testing/Turn_on_RVR_Lights.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../'))
+
+#import RVR stuff
+from sphero_sdk import SpheroRvrObserver
+from sphero_sdk import Colors
+from sphero_sdk import RvrLedGroups
+from sphero_sdk import DriveFlagsBitmask #not sure what this does
+
+rvr = SpheroRvrObserver()
+
+#function to change lights
+def LightsChange(red,green,blue):
+     rvr.set_all_leds(
+        led_group=RvrLedGroups.all_lights.value,
+        led_brightness_values=[color for _ in range(10) for color in [red,green,blue]]
+    )
+    
+try:
+    #this is code that tells the rvr what to do
+    rvr.wake()
+    time.sleep(2)
+    print('Rvr Online')
+    LightsChange(0,0,0)
+    time.sleep(1)
+    LightsChange(255,0,0)
+    time.sleep(1)
+    
+   
+    
+except KeyboardInterrupt:
+    print('\nProgram terminated with keyboard interrupt')
+
+#closes the connection to the rvr
+rvr.close() 
