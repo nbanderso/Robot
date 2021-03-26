6500120c78940a6a2cdb0b883de8233b6033cb2c
nbanderso <52547554+nbanderso@users.noreply.github.com>
2021-03-26 14:17:37 -0700

    Create Useful_RVR_Imports.py

---
+++ Testing/Useful_RVR_Imports.py
@@ -0,0 +1,14 @@
+from sphero_sdk import SpheroRvrObserver #imports RVR Observer - All Observer Functions need this
+from sphero_sdk import RvrStreamingServices #imports Color Sensor (Color Detection) and other Sensor Information
+from sphero_sdk import RawMotorModesEnum #imports Raw Motor Driving
+from sphero_sdk import DriveFlagsBitmask #imports Driving using Headers (by angle driving)
+
+#used for setting all Leds
+from sphero_sdk import Colors
+from sphero_sdk import RvrLedGroups
+
+#used for setting multiple of Leds
+from sphero_sdk import Colors
+from sphero_sdk import RvrLedGroups
+
+from sphero_sdk import BatteryVoltageStatesEnum as VoltageStates #used to get battery state
